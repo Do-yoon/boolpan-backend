eb75f2440986c027654aefff4bc822c72bc4c33b
Do-yoon <cheeezing_u@naver.com>
2021-10-30 19:16:33 +0900

    setting configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,5 @@
 import { Configuration as WebpackConfiguration } from "webpack";
 import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
-import 'webpack-dev-server';
 
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
@@ -14,11 +13,10 @@ interface Configuration extends WebpackConfiguration {
 const config: Configuration = {
     entry: {
         index: {
-            import: './src/index.tsx',
-            dependOn: 'shared',
+            import: './src/app.ts',
         },
-        shared: ['react', './src/store'],
     },
+    target: 'node',
     module: {
         rules: [
             {
@@ -28,21 +26,15 @@ const config: Configuration = {
                     loader: 'babel-loader',
                     options: {
                         presets: [
-                            ['@babel/preset-env', { targets: "defaults" }],
-                            "@babel/preset-react"
+                            ['@babel/preset-typescript', { targets: "defaults" }],
                         ],
-                        plugins: ["@babel/plugin-transform-react-jsx"]
                     }
                 }
             },
             {
-                test: /\.(tsx|ts)?$/,
+                test: /\.ts?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
-            {
-                test: /\.(jsx)?$/,
-                use: 'jsx-loader'
             },
             {
                 test: /\.css$/,
@@ -61,7 +53,6 @@ const config: Configuration = {
                         limit: 10000
                     }
                 },
-
             }
         ],
     },
@@ -80,26 +71,23 @@ const config: Configuration = {
         runtimeChunk: 'single',
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        extensions: [".ts", ".js", ".json"],
         plugins: [new TsconfigPathsPlugin({ configFile: 'src/tsconfig.json' })]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
         })
-    ],
+    ]
 };
 
-module.exports = () => {
-    if (isProduction) {
-        return {
-            ...config,
-            mode: 'production'
-        };
-    } else {
-        return {
+module.exports = (isProduction
+    ? {
+        ...config,
+        mode: 'production'
+    }
+    : {
             ...config,
             mode: 'development'
-        };
     }
-};
+    );
