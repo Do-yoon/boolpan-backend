d2c41415cc31b029e9f382b903689162c038bdac
Do-yoon <do.yoon.like.cheeze@gmail.com>
2021-11-14 22:36:59 +0900

    setting configuration

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,13 @@
-const routes = () =>  {
+const router = require('express').Router();
+const URI = require('./uri');
+const USER_URI = require('uri').USER_URL;
 
-}
-export default routes;
+router.get(URI.CHAT_ROOM_LIST, (req: any, res: any) => {
+    res.send([{
+        name: "test",
+        limit: 100,
+        current: 1
+    }]);
+});
+
+export default router;
