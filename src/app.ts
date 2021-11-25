c31b2838e44e5fd3fd09ea36c17056be59b70338
Do-yoon <cheeezing_u@naver.com>
2021-11-25 16:40:28 +0900

    add controllers

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const logger = winston.createLogger({
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.set("port", 3000);
+app.set("port", 8080);
 
 app.get('/', (req: any, res: any) => {
     res.send("Hello world");
