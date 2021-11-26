3756018351e596098a0debbdc470c60303f60440
Do-yoon <do.yoon.like.cheeze@gmail.com>
2021-11-26 16:57:19 +0900

    add cors middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import chatRouter from "./controller/ChatRouter";
 
 const router = express.Router();
+const cors = require('cors');
+
+//router.get('/', cors(), (req, res) => { res.send('cors!') });
 
 router.use('/chat', chatRouter);
 
