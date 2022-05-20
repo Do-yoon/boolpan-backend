a987ea2e2400a77d9d987c465963109a20361169
Do-yoon <do.yoon.like.cheeze@gmail.com>
2022-05-20 16:55:03 +0900

    add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // dependencies
-import express from "express";
+const express = require("express");
 import winston from "winston";
 import bodyParser from "body-parser";
 import controller from "./api";
@@ -28,29 +28,7 @@ const server = require('http').createServer(app);
 //require("dotenv").config();
 const PORT = process.env.PORT || 8081;
 console.log(`port: ${PORT}`)
-const io = require('socket.io')(server, {
-    cors: {
-        origin: ["http://localhost:3000/", "http://boolpan-frontend.s3-website.ap-northeast-2.amazonaws.com/"],
-        credentials: true
-    }
-});
 
-
-console.log("chat server");
-io.on('connection', function (socket: any) {
-
-    console.log("a user connected");
-
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-
-
-    socket.on('chatMessage', function (msg: any) {
-        console.log('message: ' + msg);
-        socket.broadcast.emit('message', msg);
-    });
-});
 
 
 // middleware
@@ -78,7 +56,7 @@ app.use(cors('mongodb://mongo:27017'));
 
 const MONGO_URL = process.env.NODE_ENV === 'production'
     ? 'mongodb://mongo:27017'
-    : 'http://localhost:27017'
+    : 'mongodb://localhost:27017'
 
 mongoose.connect(MONGO_URL + "/my_database")
     .then(() => console.log("database link success"))
@@ -94,10 +72,33 @@ app.use('/v0', controller);
 
 
 // app.use('io', io);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: ["http://localhost:3000/", "http://boolpan-frontend.s3-website.ap-northeast-2.amazonaws.com/:3000"],
+        credentials: true
+    }
+});
 
-
-server.listen(PORT, () => {
+io.listen(PORT, () => {
     console.log("http://localhost:" + PORT);
+});
+
+
+
+
+console.log("chat server");
+io.on('connection', function (socket: any) {
+
+    console.log("a user connected");
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chatMessage', function (msg: any) {
+        console.log('message: ' + msg);
+        socket.broadcast.emit('message', msg);
+    });
 });
 
 
