a825569e1cbe7baa7d577f6a627a176d0bdc7851
Do-yoon <do.yoon.like.cheeze@gmail.com>
2022-08-17 16:54:38 +0900

    socket.io 개선

--- src/api/controller/type/index.ts
+++ src/api/controller/type/index.ts
@@ -7,13 +7,7 @@ export type RoomType = {
     explode_time: number
 }
 
-export type RoomInfoType = {
-    // 익명 유저 소켓 아이디와 방 이름을 매핑
-    anonymous_user_number: Map<string, number>
-    explode_time: Number
-}
-
-export interface UserInfo {
+export type UserType = {
     user_id: string
 }
 
